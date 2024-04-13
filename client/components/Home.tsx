61bd4ed14de2993b10bb1c2c978034a99f75efcb
Maddy Rio <madison-rio@hotmail.com>
2024-04-13 18:12:09 +1200

    MVP finished for challenge board

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,9 +1,40 @@
+import Footer from './Footer'
+import { Container, Row, Col } from 'react-bootstrap';
+
+function FillSquare(square: HTMLDivElement) {
+    const currentColor = square.style.backgroundColor;
+    if (currentColor === "" || currentColor === "white") {
+        square.style.backgroundColor = "#47739f96"; // Toggle on
+    } else {
+        square.style.backgroundColor = "white"; // Toggle off
+    }
+}
 
 function Home() {
+
+    const gridItems = [...Array(75).keys()];
+
     return (
-        <main>
-            <h1>Home</h1>
-        </main>
+        <>
+        <div>
+           
+<h1>Your 75 Day Challenge</h1>
+</div>
+      <Container>
+      <div className="grid-container">
+                    {gridItems.map(index => (
+                        <div onClick={(event) => {
+                            const square = event.currentTarget as HTMLDivElement;
+                            FillSquare(square);
+                        }} key={index} className="grid-item">
+                            {index + 1}
+                        </div>
+                    ))}
+                </div>
+    </Container>
+
+    <Footer/>
+    </>
     )
 }
 
