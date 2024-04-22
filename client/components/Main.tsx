a1025d4b4d347b6d7c98ed9bc9f5e51805f542c7
Maddy Rio <madison-rio@hotmail.com>
2024-04-22 20:16:54 +1200

    testing username output

--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -30,7 +30,7 @@ return (
         <div className='header'>
       <img className="user-img" src={user?.picture} alt="User" />
       <div className="dropdown">
-            <p className="user">{user?.nickname}</p>
+            <p className="user">{user?.preferred_username}</p>
   <div className="dropdown-content">
    
     <a><LogoutButton /></a>
