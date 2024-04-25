0564abd65c7e19c5e746689749fb7bd7069493e7
Maddy Rio <madison-rio@hotmail.com>
2024-04-25 19:02:16 +1200

    Small footer edit

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,7 +1,7 @@
 function footer() {
  return (
     <section id="section-footer">
-        <p className="footer">© 2024 | Designed & coded by Maddy Rio </p>
+        <p className="footer">© 2024 | Designed & coded by Maddy Rio | Logos created with Canva</p>
     </section>
  )
 }
