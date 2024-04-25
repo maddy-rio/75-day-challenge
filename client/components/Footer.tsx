37cacfcc367e163bbd1da95bef834c1894ff94e5
Maddy Rio <madison-rio@hotmail.com>
2024-04-25 19:12:46 +1200

    Final footer edit

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function footer() {
  return (
     <section id="section-footer">
-        <p className="footer">© 2024 | Designed & coded by Maddy Rio | Logos created with <strong><Link style={{color: "black"}} to="https://www.canva.com/">Canva</Link></strong></p>
+        <p className="footer">© 2024 | Designed & coded by Maddy Rio | Logos created with <strong><Link style={{color: "black"}} to="https://www.canva.com/" target='_blank'>Canva</Link></strong></p>
     </section>
  )
 }
