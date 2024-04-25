a3a1ab9d6f87109cc1c8d8ea252cb1a9581b0c80
Maddy Rio <madison-rio@hotmail.com>
2024-04-25 19:09:18 +1200

    Small footer edit

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
+
 function footer() {
  return (
     <section id="section-footer">
-        <p className="footer">© 2024 | Designed & coded by Maddy Rio | Logos created with Canva</p>
+        <p className="footer">© 2024 | Designed & coded by Maddy Rio | Logos created with <strong><Link to="https://www.canva.com/">Canva</Link></strong></p>
     </section>
  )
 }
