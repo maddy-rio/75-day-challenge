22e1d42740261de9316004864660ab8119d4b9f1
Maddy Rio <madison-rio@hotmail.com>
2024-05-25 22:20:37 +1200

    Fix callback url

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const SignupButton = () => {
   const signupUrl = "https://dev-maddy.au.auth0.com/authorize?" +
     "response_type=code&" +
-    "client_id=h17OK32m9T9BtXdQsHjOUO7TdfnfgecB&" +
-    "redirect_uri=https://localhost:5173&" +
+    "client_id=h17OK32m9T9BtXdQsHjOUO7TdfnfgecB" +
+    "redirect_uri=https://75daychallenge.netlify.app&" +
     "state=STATE&" +
     "screen_hint=signup";
 
