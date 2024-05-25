6b811583f47e1690d9426c6b91a8ee79930af8c7
Maddy Rio <madison-rio@hotmail.com>
2024-05-26 11:46:18 +1200

    Fix sign up button url

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SignupButton = () => {
-  const signupUrl = "https://dev-maddy.au.auth0.com/u/signup?" +
-    "state=hKFo2SBvNXZ5NWtKMGRZcHRqbHdMNWJkMlFGUE1BZklrOGlCYqFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIENpYWNUOUhkczJVdnM0T3NyaEd1RUw3anFlNDhhZFpKo2NpZNkgaDE3T0szMm05VDlCdFhkUXNIak9VTzdUZGZuZmdlY0I";
+  const signupUrl = 
+  "https://dev-maddy.au.auth0.com/authorize?" +
+  "response_type=code&" +
+  "client_id=h17OK32m9T9BtXdQsHjOUO7TdfnfgecB&" +
+  "redirect_uri=https://75daychallenge.netlify.app&" +
+  "scope=SCOPE&" +
+  "state=STATE&" +
+  "screen_hint=signup";
 
   return (
     <button className="sign-up-button">
